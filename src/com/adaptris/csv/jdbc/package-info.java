57a107ce693ff4109c0aaf2c2424c127ce0659a0
Lewin Chan <lewin.chan@adaptris.com>
2017-06-19 09:42:55 +0100

    Added javadocs.

--- src/com/adaptris/csv/jdbc/package-info.java
+++ src/com/adaptris/csv/jdbc/package-info.java
@@ -1,4 +1,5 @@
 /**
- * 
+ * Implementation of {@link com.adaptris.core.services.jdbc.ResultSetTranslator} which writes a the results from a database query as
+ * CSV.
  */
 package com.adaptris.csv.jdbc;
