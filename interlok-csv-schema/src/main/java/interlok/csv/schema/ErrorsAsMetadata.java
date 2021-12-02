17a0e47d74380e9d661c9c0ee37eb235bbca5e3a
Lewin Chan <lewin.chan@adaptris.com>
2021-12-02 11:32:10 +0000

    Add csv-schema module
    - Add CSV Schema module based on reference impl.

---
+++ interlok-csv-schema/src/main/java/interlok/csv/schema/ErrorsAsMetadata.java
@@ -0,0 +1,28 @@
+package interlok.csv.schema;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.interlok.InterlokException;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import java.util.Collection;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import uk.gov.nationalarchives.csv.validator.api.java.FailMessage;
+
+/**
+ * Record all CSV Schema validation error messages as metadata.
+ * <p>This will discard all {@code WarningMessage} objects; if you want to include warnings and errors, then use {@link
+ * FailuresAsMetadata} instead.</p>
+ */
+@Slf4j
+@NoArgsConstructor
+@XStreamAlias("csv-schema-errors-as-metadata")
+public class ErrorsAsMetadata extends ViolationsAsMetadata {
+
+  public void handle(Collection<FailMessage> failures, AdaptrisMessage msg) throws InterlokException {
+    Collection<FailMessage> errors = FailureType.Error.filter(failures);
+    if (errors.size() > 0) {
+      log.trace("Recording {} errors against [{}]", errors.size(), getMetadataKey());
+      msg.addMessageHeader(getMetadataKey(), toString(errors));
+    }
+  }
+}
---
+++ interlok-csv-schema/src/main/java/interlok/csv/schema/FailuresAsMetadata.java
@@ -0,0 +1,26 @@
+package interlok.csv.schema;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.interlok.InterlokException;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import java.util.Collection;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import uk.gov.nationalarchives.csv.validator.api.java.FailMessage;
+
+/** Record all CSV Schema validation failure messages as metadata.
+ *
+ */
+@Slf4j
+@NoArgsConstructor
+@XStreamAlias("csv-schema-failures-as-metadata")
+public class FailuresAsMetadata extends ViolationsAsMetadata {
+
+  @Override
+  public void handle(Collection<FailMessage> failures, AdaptrisMessage msg) throws InterlokException {
+    if (failures.size() > 0) {
+      log.trace("Recording {} failures against [{}]", failures.size(), getMetadataKey());
+      msg.addMessageHeader(getMetadataKey(), toString(failures));
+    }
+  }
+}
